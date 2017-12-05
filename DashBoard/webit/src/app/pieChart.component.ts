d92b2b8f6ac5e59f3e6350d61266b497ebfe628a
ayalaswebit <33722689+ayalaswebit@users.noreply.github.com>
2017-12-05 09:57:42 +0200

    שינוי צבע

--- DashBoard/webit/src/app/pieChart.component.ts
+++ DashBoard/webit/src/app/pieChart.component.ts
@@ -15,6 +15,7 @@ import {Component} from "@angular/core";
             <canvas baseChart
                     [data]="pieChartData"
                     [labels]="pieChartLabels"
+                    [colors]="lineChartColors"
                     [chartType]="pieChartType"
                     (chartHover)="chartHovered($event)"
                     (chartClick)="chartClicked($event)"></canvas>
@@ -37,5 +38,31 @@ export class PieChartComponent {
     public chartHovered(e: any): void {
         console.log(e);
     }
+    public lineChartColors:Array<any> = [
+        { // grey
+            backgroundColor: 'rgba(148,159,177,0.2)',
+            borderColor: 'rgba(148,159,177,1)',
+            pointBackgroundColor: 'rgba(148,159,177,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+        },
+        { // dark grey
+            backgroundColor: 'rgba(77,83,96,0.2)',
+            borderColor: 'rgba(77,83,96,1)',
+            pointBackgroundColor: 'rgba(77,83,96,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(77,83,96,1)'
+        },
+        { // grey
+            backgroundColor: 'rgba(148,159,177,0.2)',
+            borderColor: 'rgba(148,159,177,1)',
+            pointBackgroundColor: 'rgba(148,159,177,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+        }
+    ];
 
 }
